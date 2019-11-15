fcf8b1af54621547d295a95d75f8a3c2340edb42
yuchang123 <15652053964@163.com>
2019-11-15 11:09:02 +0800

    我上传的项目

---
+++ src/main/java/cn/xdl/java/java.java
@@ -0,0 +1,10 @@
+package cn.xdl.java;
+
+public class java {
+
+	public static void main(String[] args) {
+		System.out.println("java");
+
+	}
+
+}
---
+++ src/test/java/com/xdl/test/test.java
@@ -0,0 +1,10 @@
+package com.xdl.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("测试");
+
+	}
+
+}
