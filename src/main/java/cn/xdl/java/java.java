6c3d226ade75a00ed02499450275ad7017c6856d
5211314wkl <3385223022@qq.com>
2019-11-15 11:33:03 +0800

    111

--- src/main/java/cn/xdl/java/java.java
+++ src/main/java/cn/xdl/java/java.java
@@ -4,7 +4,8 @@ public class java {
 
 	public static void main(String[] args) {
 		System.out.println("java");
-		system.out.println("java2 ")
+		system.out.println("java2 ");
+		System.out.println("java3");
 
 	}
 
